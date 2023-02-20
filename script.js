30c7ec799b644751ee3241879305c71d91b85374
Juliana Villegas <jumavipe@hotmail.com>
2023-02-20 09:55:17 -0500

    clase 20 feb

--- script.js
+++ script.js
@@ -56,3 +56,46 @@ function agregarItem() {
     }
 
 }
+
+//// Inner HTML
+const card = document.createElement('article')
+card.className = 'row'
+
+const imgUrl= 'https://cdn.pixabay.com/photo/2012/08/27/14/19/mountains-55067_960_720.png'
+
+card.innerHTML = `<div class="col s3">
+<div class="card">
+  <div class="card-image">
+    <img src="${imgUrl}">
+    <span class="card-title">Card Title</span>
+  </div>
+  <div class="card-content">
+    <p>I am a very simple card. I am good at containing small bits of information.
+    I am convenient because I require little markup to use effectively.</p>
+  </div>
+  <div class="card-action">
+    <a href="#">This is a link</a>
+  </div>
+</div>
+</div> `
+
+const cardsContainer = document.getElementById('cards-container')
+cardsContainer.append(card)
+
+
+/// getAttribute & setAttribute
+function cambiarAttr(clickedCard=card, imageLink = 'https://coolhuntermx.com/wp-content/uploads/2019/03/feliz.jpg'){
+
+    const cardImg = clickedCard.querySelector('.card-image img')
+    console.log(cardImg.getAttribute('src'))
+    cardImg.setAttribute('src', imageLink)
+
+
+}
+
+/// Event listeners
+const cardBtn = document.querySelector('.card-action button')
+const eventCard= cardBtn.parentElement.parentElement
+cardBtn.addEventListener('click', () => cambiarAttr(eventCard, 'https://as2.ftcdn.net/v2/jpg/04/31/82/57/1000_F_431825797_gbFmyO0sBt29VljKwxyYxDjcV5oJCHzi.jpg'))
+
+/// Array, spread operators, clone objects
